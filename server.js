b2ed1892f435a87a29a3d4089539bf5c71aea3b5
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-08 00:33:57 +0200

    Add: parametric route

--- server.js
+++ server.js
@@ -19,4 +19,17 @@ app.get("/users", async (req, res) => {
   }
 });
 
+app.get('/users/:email', async (req, res)=> {
+    try{
+        const user = await User.findOne({email: req.params.email}).populate('projects');
+        if(user){
+            res.status(200).json(user);
+        }else {
+            res.status(404).json('not found');
+        }
+    }catch(err){
+        res.status(500).json({ error: 'server error' }); 
+    }
+})
+
 
