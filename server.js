ecb0ea80d0842a6f1d9c575de3891297480d6713
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-07 19:20:11 +0200

    Add: first route implemented

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 require('dotenv').config();
+const User = require('./models/User');
 
 const app = express();
 
 
 
 app.listen(3000, async()=> {
-    console.log('running');
-})
+    console.log('running...');
+});
+
+
+app.get('/users', async (req, res)=> {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'server error'});
+    }
+});
 
 
 
