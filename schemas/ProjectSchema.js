54816ae9c28b6aeb51ce4d0287d7ffc397198d64
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-07 19:50:54 +0200

    Add: project Schema

---
+++ schemas/ProjectSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = mongoose.Schema({
+    title: {
+        type: String,
+        max: 200,
+        required: [true, 'title is required'],
+    },
+    description: {
+        type: String,
+        max: 500,
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required'],
+        validate: {
+          validator: function (value) {
+            // Use a regular expression to check if the image ends with .jpg, .png, or .svg
+            return /\.(jpg|png|svg)$/.test(value);
+          },
+          message: 'Image must be in JPG, PNG, or SVG format',
+        },
+    },
+})
+
+ProjectSchema.index({ title: 1, user: 1 }, { unique: true });
