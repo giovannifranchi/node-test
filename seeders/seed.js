40e56834ed044f0f4376365f418e9693c9194103
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-07 18:39:04 +0200

    fix: add extension to seeder

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 const filename = process.argv[2]; 
 
-exec(`node seeders/${filename}`, (error, stdout, stderr) => {
+exec(`node seeders/${filename}.js`, (error, stdout, stderr) => {
   if (error) {
     console.error('Error executing script:', error);
   } else {
