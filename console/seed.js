7ef5a7e7158ff93dbd908ccadca15d8fec070958
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-08 13:57:26 +0200

    Add: mass seeder

--- console/seed.js
+++ console/seed.js
@@ -2,10 +2,26 @@ const { exec } = require('child_process');
 
 const filename = process.argv[2]; 
 
-exec(`node seeders/${filename}.js`, (error, stdout, stderr) => {
-  if (error) {
-    console.error('Error executing script:', error);
-  } else {
-    console.log(`${filename} executed successfully`);
+//const massSeeder = ['ProjectSeeder', 'UserSeeder']; //populate in order of execution with filenames the mass seeder to execute a global seeding, if you need to populate dependecies use the single seeder
+
+
+if(filename){
+  exec(`node seeders/${filename}.js`, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error executing script:', error);
+    } else {
+      console.log(`${filename} executed successfully`);
+    }
+  });
+}else {
+
+  for(let seeder of massSeeder){
+    exec(`node seeders/${seeder}.js`, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Error executing script:', error);
+      } else {
+        console.log(`${seeder} executed successfully`);
+      }
+    })
   }
-});
+}
